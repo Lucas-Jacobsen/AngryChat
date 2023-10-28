b77a9c943caf0e2c6173d3f9d914907b9384b9df
Grant-M-Lindquist <grant-lindquist@outlook.com>
2023-10-28 11:26:22 -0700

    First UI implementation

--- src/Main.js
+++ src/Main.js
@@ -1,16 +1,58 @@
 import React from "react";
 import "./App.css";
-import { useUser } from "@clerk/clerk-react";
+
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {
+  MainContainer,
+  ChatContainer,
+  MessageList,
+  Message,
+  MessageInput,
+  Conversation,
+  ConversationList,
+} from "@chatscope/chat-ui-kit-react";
+import { Typography } from "@mui/material";
+
 
 export default function Main() {
+  return (
+    <>
+    <nav style={{height: '10vh'}}>
+      <Typography variant="h2" px={2}>AngryChat</Typography>
+    </nav>
+    <MainContainer style={{height: '90vh'}}>
+        <ConversationList>        
+          <Conversation name="Grant" lastSenderName="Grant" info="RAHHH I AM SO ANGRY!">
+ 
+          </Conversation>
+          
+          <Conversation name="Luke" lastSenderName="Luke" info="I am going to hit you with my car.">
 
-    const { user } = useUser();
+          </Conversation>
+                                              
+          <Conversation name="Ivan" lastSenderName="Ivan" info="i am so filled with rage!!!">
 
-    return(
-        <div>
-            <h1>You are signed in</h1>
-            <p>{user.firstName} {user.lastName}</p>
-            <p>UserId: {user.id}</p>
-        </div>
-    );
+          </Conversation>
+                                                          
+          <Conversation name="Noah" lastSenderName="Noah" info="its berserkin time....">
+
+          </Conversation>
+          
+        </ConversationList>
+        <ChatContainer>
+          <MessageList>
+            <Message
+              model={{
+                message: "Here is a test message",
+                sentTime: "just now",
+                sender: "Joe",
+              }}
+            />
+          </MessageList>
+          <MessageInput placeholder="Type message here" />
+        </ChatContainer>
+      </MainContainer>
+    </>
+      
+  );
 }
