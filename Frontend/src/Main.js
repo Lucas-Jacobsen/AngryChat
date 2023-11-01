22e4837bf905f54733b54cf32dccc002e9d17f93
Grant-M-Lindquist <grant-lindquist@outlook.com>
2023-11-01 08:38:12 -0700

    Added UI "endpoints" for API connection

--- Frontend/src/Main.js
+++ Frontend/src/Main.js
@@ -1,56 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
 import {
     MainContainer,
-    ChatContainer,
-    MessageList,
-    Message,
-    MessageInput,
-    Conversation,
-    ConversationList,
 } from "@chatscope/chat-ui-kit-react";
-import { Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
+import Chat from "./Chat";
+import Navbar from "./Navbar";
 
 
 export default function Main() {
+
+    // User that user is messaging
+    const [focusedUser, setFocusedUser] = useState('');
+
+    // List of user that user is friends with
+    const [userList, setUserList] = useState([]);
+
+    useEffect(() => {
+        // Get backend stuff and set states
+        setFocusedUser('');
+        setUserList([
+            {
+                name: 'Grant'
+            },
+            {
+                name: 'Luke'
+            },
+            {
+                name: 'Ivan'
+            },
+            {
+                name: 'Noah'
+            },
+        ]);
+    }, [])
+
     return (
         <>
-            <nav style={{ height: '10vh' }}>
-                <Typography variant="h2" px={2}>AngryChat</Typography>
-            </nav>
+            <Navbar/>
             <MainContainer style={{ height: '90vh' }}>
-                <ConversationList>
-                    <Conversation name="Grant" lastSenderName="Grant" info="RAHHH I AM SO ANGRY!">
-
-                    </Conversation>
-
-                    <Conversation name="Luke" lastSenderName="Luke" info="I am going to hit you with my car.">
-
-                    </Conversation>
-
-                    <Conversation name="Ivan" lastSenderName="Ivan" info="i am so filled with rage!!!">
-
-                    </Conversation>
-
-                    <Conversation name="Noah" lastSenderName="Noah" info="its berserkin time....">
-
-                    </Conversation>
-
-                </ConversationList>
-                <ChatContainer>
-                    <MessageList>
-                        <Message
-                            model={{
-                                message: "Here is a test message",
-                                sentTime: "just now",
-                                sender: "Joe",
-                            }}
-                        />
-                    </MessageList>
-                    <MessageInput placeholder="Type message here" />
-                </ChatContainer>
+                <Sidebar userList={userList} setFocusedUser={setFocusedUser}/>
+                <Chat focusedUser={focusedUser}/>
             </MainContainer>
         </>
 
