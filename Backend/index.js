4ef5b14511a6868f1887c1eff94cb905d2b79d7a
Lucas-Jacobsen <lukejacobsen33@gmail.com>
2023-11-13 21:25:30 -0700

    cors

--- Backend/index.js
+++ Backend/index.js
@@ -129,6 +129,7 @@ app.post("/users", async(req, res) => {
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 })
 
 app.listen(port, () => {
