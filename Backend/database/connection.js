0c900206827dede47dd7564cfb9470dbfe8bda1d
Grant-M-Lindquist <grant-lindquist@outlook.com>
2023-11-17 09:35:25 -0700

    test

--- Backend/database/connection.js
+++ Backend/database/connection.js
@@ -1,17 +1,16 @@
 import sql from "mysql";
 import os from "os"
 
-
 let port = 3306;
 if(os.platform() == "darwin") {
     port = 8889;
 }
 
-export let connection = sql.createConnection({
-    host     : process.env.host,
-    port     : port,
-    user     : process.env.user,
-    password : process.env.password,
-    database : process.env.database
-})
 
+    export let connection = sql.createConnection({
+        host     : "j8oay8teq9xaycnm.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port     : 3306,
+        user     : "c7tn28mg8fu5w2bz",
+        password : "tkyccz0vsu49b75z",
+        database : "c5mm0aic6ljm3kc5"
+    })
