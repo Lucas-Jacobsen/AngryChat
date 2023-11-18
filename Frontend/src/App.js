e03a3c388db838a26bb29638627646b061484f54
Lucas-Jacobsen <lukejacobsen33@gmail.com>
2023-11-17 18:57:34 -0700

    app

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import "./App.css";
-import Main from "./components/Main";
-import { SignedIn, SignedOut, SignIn, useUser } from "@clerk/clerk-react";
-=======
 import Main from "./new_components/Main";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import { SignedIn, SignedOut, SignIn, useUser } from "@clerk/clerk-react";
 import { Box, CssBaseline } from "@mui/material";
->>>>>>> 0f3555f90a99cdd7292d724dcb5b190df0bf28db
 
 function App() {
   const { user } = useUser();
