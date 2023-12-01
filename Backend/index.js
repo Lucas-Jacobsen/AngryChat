8e923e2ce82b3b928b963a13d43ef84dc8d78245
Ivan Gudino <42625556+Raptas1@users.noreply.github.com>
2023-12-01 08:15:21 -0700

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ import "./services/WebSockets.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
-const server = createServer(app);
+export const server = createServer(app);
 //import cors from 'cors';
 
 
